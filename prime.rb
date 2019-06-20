148c60cc011ccb7c3432c283c24cffb113b1c356
Rashad Walcott <rashad.walcott15@gmail.com>
2019-06-20 04:55:24 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,19 @@
 # Add  code here!
 def prime?(num)
+(2...num).each do |divisor|
+    return false if num % divisor == 0
+  end
 
+  true
+end
+
+def primes(max)
+  primes = []
+
+  (2...max).each do |num|
+    primes << num if is_prime?(num)
+  end
+
+  primes
 end
 end
