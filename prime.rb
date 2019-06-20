011af21c7f4fe9eefb56bc2af1cf3cac1dff2c86
Rashad Walcott <rashad.walcott15@gmail.com>
2019-06-20 04:47:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,10 +2,6 @@
 def prime?(num)
   #Math.sqrt(int).floor.downto(2).each {|i| return false if int % i == 0}
   # true
-  (2..Math.sqrt(num)).each { |i|
-  if num % i == 0 && i < num
-    return false
-  end
-  }
-  true
+  (2..Math.sqrt(num)).each { |i| return false if num % i == 0}
+        true
 end
