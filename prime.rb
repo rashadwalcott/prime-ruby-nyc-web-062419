6b80069984778819f77d1794a682ceade4f12b0c
Rashad Walcott <rashad.walcott15@gmail.com>
2019-06-20 04:50:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,14 +2,5 @@
 def prime?(num)
   #Math.sqrt(int).floor.downto(2).each {|i| return false if int % i == 0}
   # true
-  if number == 1 then return false end        
-
-        max = Math.sqrt(number)
-
-        (2..max).any? do |i| 
-            if number % i == 0 then return false end
-        end
-
-        true
-    end
+  
 end
